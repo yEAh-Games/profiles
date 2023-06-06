ee8a9e7e82ffcf3869f9a48d9b5999a8a3d3326b
yEAh-Games <tallinnquinton@gmail.com>
2023-06-06 07:49:11 -0400

    test

--- scripts/getprofile.js
+++ scripts/getprofile.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   var profileCard = document.getElementById("profile-card");
+  var userProfile; // Declare userProfile variable in the outer scope
 
   // Fetch the user's profile data from the JSON database
   fetch("https://accounts.yeahgames.net/data/settings/profiles.json")
@@ -14,7 +15,7 @@ document.addEventListener("DOMContentLoaded", function () {
       // Retrieve the user's profile based on the permalink (username)
       var permalink = window.location.pathname.replace("/", "");
       var username = permalink.replace("@", "").replace(".html", ""); // Remove the "@" symbol
-      var userProfile = profileData.find(function (profile) {
+      userProfile = profileData.find(function (profile) {
         return profile.username === username;
       });
 
@@ -37,7 +38,7 @@ document.addEventListener("DOMContentLoaded", function () {
             if (userAccount) {
               if (userAccount.admin) {
                 userProfile.isAdmin = true;
-                insertAdminHTML();
+                insertAdminHTML(userProfile); // Pass userProfile to the function
               } else {
                 userProfile.isAdmin = false;
               }
@@ -88,9 +89,10 @@ document.addEventListener("DOMContentLoaded", function () {
     .catch(function (error) {
       console.error("Error:", error);
     });
-  var adminsvg = '&#160;<a href="https://admin.yeahgames.net" style="text-decoration:none!important;" title="This user is a yEAh admin."><svg style="color:#ff4747!important;" class="VQS2tmQ_zFyBOC2tkmto YIUegm7fh_CpJbivTu6B MnxxlQlR1H0xJuMEE8Yr PeR2JZ9BZHYIH8Ea3F36 bcsWqjK52oeyT6oeC2Az gZ3KuFw1JESHhOJhjT8j _Oyukq8JlN1X9w2FmPds XIIs8ZOri3wm8Wnj9N_y Lld6j9B1iilEqA6j31e4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd"></path></svg></a>'
+  
+  var adminsvg = '<a href="https://admin.yeahgames.net" style="text-decoration:none!important;" title="This user is a yEAh admin."><svg style="color:#ff4747!important;" class="VQS2tmQ_zFyBOC2tkmto YIUegm7fh_CpJbivTu6B MnxxlQlR1H0xJuMEE8Yr PeR2JZ9BZHYIH8Ea3F36 bcsWqjK52oeyT6oeC2Az gZ3KuFw1JESHhOJhjT8j _Oyukq8JlN1X9w2FmPds XIIs8ZOri3wm8Wnj9N_y Lld6j9B1iilEqA6j31e4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd"></path></svg></a>';
 
-  function insertAdminHTML() {
+  function insertAdminHTML(userProfile) {
     var profileAdminElement = document.getElementById('profileAdmin');
     if (userProfile.isAdmin) {
       profileAdminElement.innerHTML = adminsvg;
@@ -98,5 +100,4 @@ document.addEventListener("DOMContentLoaded", function () {
       profileAdminElement.innerHTML = "";
     }
   }
-
 });
