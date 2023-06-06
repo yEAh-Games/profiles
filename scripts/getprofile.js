2a39354ace32372af8e0c616b3ff7558af50be73
yEAh-Games <tallinnquinton@gmail.com>
2023-06-05 23:04:30 -0400

    stuff

--- scripts/getprofile.js
+++ scripts/getprofile.js
@@ -34,11 +34,17 @@ document.addEventListener("DOMContentLoaded", function () {
               return account.username === userProfile.username;
             });
 
-            if (userAccount && userAccount.admin) {
-              userProfile.isAdmin = true;
+            if (userAccount) {
+              if (userAccount.admin) {
+                userProfile.isAdmin = true;
+                insertAdminHTML();
+              } else {
+                userProfile.isAdmin = false;
+              }
             } else {
               userProfile.isAdmin = false;
             }
+
             var img = document.getElementById('profileImage');
             var url = 'https://ugc.yeahgames.net/profile/p/default/png/@' + userProfile.username + '.png';
             var defaultUrl = 'https://ugc.yeahgames.net/profile/p/default/png/default.png';
@@ -47,7 +53,6 @@ document.addEventListener("DOMContentLoaded", function () {
             };
 
             img.src = url;
-
 
             var profileNameElement = document.getElementById('profileName');
             profileNameElement.textContent = userProfile.name;
@@ -67,9 +72,15 @@ document.addEventListener("DOMContentLoaded", function () {
     .catch(function (error) {
       console.error("Error:", error);
     });
+  var adminsvg = '<a href="https://admin.yeahgames.net" style="text-decoration:none!important;" title="This user is a yEAh admin."><svg style="color:#ff4747!important;" class="VQS2tmQ_zFyBOC2tkmto YIUegm7fh_CpJbivTu6B MnxxlQlR1H0xJuMEE8Yr PeR2JZ9BZHYIH8Ea3F36 bcsWqjK52oeyT6oeC2Az gZ3KuFw1JESHhOJhjT8j _Oyukq8JlN1X9w2FmPds XIIs8ZOri3wm8Wnj9N_y Lld6j9B1iilEqA6j31e4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd"></path></svg></a>'
 
-
-
+  function insertAdminHTML() {
+    var profileAdminElement = document.getElementById('profileAdmin');
+    if (userProfile.isAdmin) {
+      profileAdminElement.innerHTML = adminsvg;
+    } else {
+      profileAdminElement.innerHTML = "";
+    }
+  }
 
 });
-
